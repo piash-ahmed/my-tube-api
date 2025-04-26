613510c7c4eb9e4dc69716ed85f456df9d81142d
piash-ahmed <aryanpiash420@gmail.com>
2025-04-26 10:01:47 +0600

    added dynamic category buttons from fetched api

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('connected');
+const loadCategoryButtons = async() => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/phero-tube/categories`)
+    const data = await response.json()
+    console.log(data.categories);
+    const categoryContainer = document.getElementById('category-container')
+
+    data.categories.forEach(item => {
+        console.log(item.category);
+      const categoryBtn = document.createElement('div')
+      categoryBtn.innerHTML = `
+      <button class="btn rounded-full px-6">${item.category}</button>
+      `
+        categoryContainer.append(categoryBtn)
+    });
+    
+}
+loadCategoryButtons()
